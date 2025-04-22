48ab744a1e2debffd2fd7de230e5d4ba3c4264f7
Arpit Raj <arpitraj9210@gmail.com>
2025-04-23 00:26:49 +0530

    test2

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ document.addEventListener("DOMContentLoaded", function () {
             const response = await fetch("http://127.0.0.1:8000/ask", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ question }),
+                body: JSON.stringify({ question: englishQuestion }),
             });
     
             if (!response.ok) {
@@ -121,4 +121,3 @@ recognition.onend = function () {
     console.log("Voice recognition stopped.");
 };
 }
-hiiii
